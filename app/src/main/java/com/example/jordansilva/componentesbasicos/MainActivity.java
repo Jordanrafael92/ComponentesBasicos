9a438a16056d5ea3fcc34752afabc953bd2bc6fa
Jordan Silva <jordanrafael92@gmail.com>
2018-06-30 14:21:38 -0300

    Acrescentado 3 checkBox, e cada vez que selecionava e clicava no botão enviar, o Text recebia o texto de acordo com o checkBox selecionado.

--- app/src/main/java/com/example/jordansilva/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/example/jordansilva/componentesbasicos/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText campoProduto;
     private TextView resultado;
+    private CheckBox checkBranco, checkVerde, checkVermelho ;
+    List<String> check = new ArrayList<String>();
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -18,10 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
         campoProduto = findViewById(R.id.etNomeProduto);
         resultado = findViewById(R.id.tvResultado);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVerde = findViewById(R.id.checkVerde);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+
+    public void verificaCheck(){
+        check.clear();
+
+        if ( checkBranco.isChecked() ) {
+            check.add( checkBranco.getText().toString() );
+        }
+
+        if ( checkVerde.isChecked() ) {
+            check.add( checkVerde.getText().toString() );
+        }
+
+        if ( checkVermelho.isChecked() ) {
+            check.add( checkVermelho.getText().toString() );
+        }
+
+        resultado.setText( check.toString() );
+
     }
 
     public void btEnviar (View view){
         String nomeProduto = campoProduto.getText().toString();
         resultado.setText(nomeProduto);
+
+        verificaCheck(); //chamando o método
     }
 }
